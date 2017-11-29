7fc97edbcf95b90c0d34ba18fad46abf63155fc6
MaT1g3R <mat1g3r@gmail.com>
2017-11-28 23:37:52 -0500

    Add some db models

--- expense_snek_api/api/models.py
+++ expense_snek_api/api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+class TimedMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Share(TimedMixin):
+    name = models.CharField(unique=True, max_length=64)
+    description = models.CharField(max_length=256)
+    users = models.ManyToManyField(User)
+
+
+class User(TimedMixin):
+    name = models.CharField(unique=True, max_length=64)
+    expenses = models.ManyToManyField(Expense)
+
+
+class ExpenseRatio(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    numerator = models.PositiveIntegerField()
+    denominator = models.PositiveIntegerField()
+    expense = models.ForeignKey(Expense, on_delete=models.CASCADE)
+
+
+class Expense(TimedMixin):
+    description = models.CharField(max_length=256)
+    share = models.ForeignKey(Share, on_delete=models.CASCADE)
+    total = models.FloatField()
+    paid_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    resolved = models.BooleanField(default=False)
--- expense_snek_api/expense_snek_api/settings.py
+++ expense_snek_api/expense_snek_api/settings.py
@@ -46,6 +46,8 @@ ALLOWED_HOSTS = ['web']
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
